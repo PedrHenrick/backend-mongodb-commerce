6f1668d875137e8fe25093c66cd6b72fe796ee09
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-16 15:59:37 -0300

    feat(desafio): :sparkles: Criando desafio 10
    Retornando lancges que tenham o percentual de proteínas maior ou igual a 30 e menor ou igual a 40

---
+++ challenges/desafio10.js
@@ -0,0 +1,10 @@
+db.produtos.find(
+  { 
+    $and: [{ 
+      valoresNutricionais: { $elemMatch: { tipo: "proteínas", percentual: { $gte: 30 } } },
+    }, {
+      valoresNutricionais: { $elemMatch: { tipo: "proteínas", percentual: { $lte: 40 } } },
+    }], 
+  },
+  { _id: false, nome: true },
+);
